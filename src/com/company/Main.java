67ee26158fd90176c1e757d16192f7947bc3da67
Viki <vityamozer@gmail.com>
2021-03-23 12:13:05 +0200

    Test 26.02 class Test

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+class Test{}  //1
+public class TestClass {
+
+    public int main(String[] args) {  //2
+	double x = 10,double y;             //3
+        System.out.println();           //4
+        for (int k = 0; k < x; k++) {   //5
+
+        }return 0;
+    }
+}
+//3 and 4 invalid
